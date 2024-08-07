0d179a18787e93e9680e8ca856de61d8767c4736
Зураб <129601088+shakhbanov@users.noreply.github.com>
2024-08-08 02:48:12 +0300

    First commit

---
+++ shakhbanov/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shakhbanov_ml',
+    version='0.1.0',
+    author='Zurab Shakhbanov',
+    author_email='zurab@shakhbanov.ru',
+    description='ML tools for time series',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://shakhbanov.org',
+    project_urls={
+        'GitHub': 'https://github.com/shakhbanov/shakhbanov_ml',
+    },
+    download_url='https://github.com/shakhbanov/shakhbanov_ml/archive/refs/tags/v0.1.0.tar.gz',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy==1.24',
+        'pandas',
+        'matplotlib',
+        'tqdm',
+        'typing',
+        'joblib',
+        'prophet',
+        'lightgbm',
+        'holidays',
+        'sklearn',
+        'pykalman',
+    ],
+)
---
+++ shakhbanov/shakhbanov/kalman_gap_filler/__init__.py
@@ -0,0 +1,5 @@
+# shakhbanov/kalman_gap_filler/__init__.py
+
+from .kalman_gap_filler import KalmanGapFiller
+
+__all__ = ['KalmanGapFiller']
