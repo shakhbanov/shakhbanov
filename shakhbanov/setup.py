5096ca9e99e5916dc2f3c989714f2d3a45dfd406
Зураб <129601088+shakhbanov@users.noreply.github.com>
2024-08-08 03:24:29 +0300

    Update setup.py

--- shakhbanov/setup.py
+++ shakhbanov/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shakhbanov_ml',
-    version='0.1.0',
+    version='0.2.0',
     author='Zurab Shakhbanov',
     author_email='zurab@shakhbanov.ru',
     description='ML tools for time series',
