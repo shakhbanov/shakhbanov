87c8afa400dd7279fb396c74bbdd45f48177eaec
Зураб <129601088+shakhbanov@users.noreply.github.com>
2024-08-11 23:26:39 +0300

    Delete shakhbanov directory

--- shakhbanov/shakhbanov/metrics/__init__.py
+++
@@ -1,9 +0,0 @@
-from .metrics import (
-    accuracy, precision, recall, f_score, roc_auc,
-    mae, mse, rmse, mape, wape, smape, mase
-)
-
-__all__ = [
-    'accuracy', 'precision', 'recall', 'f_score', 'roc_auc',
-    'mae', 'mse', 'rmse', 'mape', 'wape', 'smape', 'mase'
-]
